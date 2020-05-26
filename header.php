ae8e531cc3c76fa0d204b5250a93329b173c48bb
Jeremy Francis <jfrancisrws@gmail.com>
2020-05-26 13:37:53 -0400

    Update to handle long links breaking pages

--- header.php
+++ header.php
@@ -41,6 +41,15 @@
 		}
 		a {
 			text-decoration:none;
+			overflow-wrap: break-word;
+		    	word-wrap: break-word;
+		    	-ms-word-break: break-all;
+    			word-break: break-all;
+    			word-break: break-word;
+    			-ms-hyphens: auto;
+    			-moz-hyphens: auto;
+    			-webkit-hyphens: auto;
+    			hyphens: auto;
 		}
 		.animated {
 			opacity:0;
